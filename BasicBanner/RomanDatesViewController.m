e16bdc5067cf651b4ab17534b3b399425962dc86
Rob Clarke <kumo@kumo.it>
2014-08-25 18:50:07 +0200

    containers and banners resize properly

--- BasicBanner/RomanDatesViewController.m
+++ BasicBanner/RomanDatesViewController.m
@@ -46,17 +46,12 @@
  */
 
 #import "RomanDatesViewController.h"
+#import "BannerViewController.h"
 #import "Converter.h"
 #import "RomanNumsActivityItemProvider.h"
 
 @interface RomanDatesViewController ()
 
-@property (nonatomic, strong) IBOutlet UIView *contentView;
-
-// contentView's vertical bottom constraint, used to alter the contentView's vertical size when ads arrive
-@property (nonatomic, strong) IBOutlet NSLayoutConstraint *bottomConstraint;
-
-@property (nonatomic, strong) IBOutlet UILabel *timerLabel;
 @property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
 @property (weak, nonatomic) IBOutlet UILabel *dateLabel;
 
@@ -67,87 +62,32 @@
 
 @implementation RomanDatesViewController
 {
-    ADBannerView *_bannerView;
-    NSTimer *_timer;
-    CFTimeInterval _ticks;
 }
 
-- (void)layoutAnimated:(BOOL)animated
+- (instancetype)init
 {
-    CGRect contentFrame = self.view.bounds;
-
-    // all we need to do is ask the banner for a size that fits into the layout area we are using
-    CGSize sizeForBanner = [_bannerView sizeThatFits:contentFrame.size];
-    
-    // compute the ad banner frame
-    CGRect bannerFrame = _bannerView.frame;
-    if (_bannerView.bannerLoaded) {
-        
-        // bring the ad into view
-        contentFrame.size.height -= sizeForBanner.height;   // shrink down content frame to fit the banner below it
-        bannerFrame.origin.y = contentFrame.size.height;
-        bannerFrame.size.height = sizeForBanner.height;
-        bannerFrame.size.width = sizeForBanner.width;
-        
-        // if the ad is available and loaded, shrink down the content frame to fit the banner below it,
-        // we do this by modifying the vertical bottom constraint constant to equal the banner's height
-        //
-        NSLayoutConstraint *verticalBottomConstraint = self.bottomConstraint;
-        verticalBottomConstraint.constant = sizeForBanner.height;
-        [self.view layoutSubviews];
-        
-    } else {
-        // hide the banner off screen further off the bottom
-        bannerFrame.origin.y = contentFrame.size.height;
+    self = [super initWithNibName:@"RomanDatesViewController" bundle:nil];
+    if (self) {
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(willBeginBannerViewActionNotification:) name:BannerViewActionWillBegin object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didFinishBannerViewActionNotification:) name:BannerViewActionDidFinish object:nil];
     }
-
-    [UIView animateWithDuration:animated ? 0.25 : 0.0 animations:^{
-        _contentView.frame = contentFrame;
-        [_contentView layoutIfNeeded];
-        _bannerView.frame = bannerFrame;
-    }];
+    return self;
 }
 
-- (void)startTimer
+- (void)dealloc
 {
-    if (_timer == nil) {
-        _timer = [NSTimer scheduledTimerWithTimeInterval:0.1
-                                                  target:self
-                                                selector:@selector(timerTick:)
-                                                userInfo:nil
-                                                 repeats:YES];
-    }
-}
-
-- (void)stopTimer
-{
-    [_timer invalidate];
-    _timer = nil;
-}
-
-- (void)timerTick:(NSTimer *)timer
-{
-    // Timers are not guaranteed to tick at the nominal rate specified, so this isn't technically accurate.
-    // However, this is just an example to demonstrate how to stop some ongoing activity, so we can live with that inaccuracy.
-    _ticks += 0.1;
-    double seconds = fmod(_ticks, 60.0);
-    double minutes = fmod(trunc(_ticks / 60.0), 60.0);
-    double hours = trunc(_ticks / 3600.0);
-    self.timerLabel.text = [NSString stringWithFormat:@"%02.0f:%02.0f:%04.1f", hours, minutes, seconds];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
-    // On iOS 6 ADBannerView introduces a new initializer, use it when available.
-    if ([ADBannerView instancesRespondToSelector:@selector(initWithAdType:)]) {
-        _bannerView = [[ADBannerView alloc] initWithAdType:ADAdTypeBanner];
-    } else {
-        _bannerView = [[ADBannerView alloc] init];
-    }
-    _bannerView.delegate = self;
-    [self.view addSubview:_bannerView];
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskAll;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -159,7 +99,6 @@
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
-    [self layoutAnimated:NO];
     //[self startTimer];
 }
 
@@ -176,36 +115,15 @@
 }
 #endif
 
-- (NSUInteger)supportedInterfaceOrientations
+
+- (void)willBeginBannerViewActionNotification:(NSNotification *)notification
 {
-    return UIInterfaceOrientationMaskAll;
+//    [self stopTimer];
 }
 
-- (void)viewDidLayoutSubviews
+- (void)didFinishBannerViewActionNotification:(NSNotification *)notification
 {
-    [self layoutAnimated:[UIView areAnimationsEnabled]];
-}
-
-- (void)bannerViewDidLoadAd:(ADBannerView *)banner
-{
-    [self layoutAnimated:YES];
-}
-
-- (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
-{
-    NSLog(@"didFailToReceiveAdWithError %@", error);
-    [self layoutAnimated:YES];
-}
-
-- (BOOL)bannerViewActionShouldBegin:(ADBannerView *)banner willLeaveApplication:(BOOL)willLeave
-{
-    //[self stopTimer];
-    return YES;
-}
-
-- (void)bannerViewActionDidFinish:(ADBannerView *)banner
-{
-    //[self startTimer];
+//    [self startTimer];
 }
 
 #pragma mark - Actions
