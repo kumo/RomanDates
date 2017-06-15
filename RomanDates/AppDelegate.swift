06235b2171e34625233770f01c725aedb1de7b05
Rob Clarke <kumo@kumo.it>
2017-06-15 14:51:54 +0200

    re-enabling Fabric

--- RomanDates/AppDelegate.swift
+++ RomanDates/AppDelegate.swift
@@ -35,7 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        //Fabric.with([Crashlytics.self])
+        Fabric.with([Crashlytics.self])
         
         AppConfiguration.sharedConfiguration.runHandlerOnFirstLaunch {
             // do nothing
